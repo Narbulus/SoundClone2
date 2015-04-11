337ec2aa324243d36a82a4383fdebcff2c244aab
Spencer Anderson <s.hughes.anderson@gmail.com>
2015-04-11 05:28:25 -0700

    Missed a spot

---
+++ src/main/java/net/technicpack/launcher/ui/components/songs/LabelProgressBar.java
@@ -0,0 +1,45 @@
+package net.technicpack.launcher.ui.components.songs;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JLabel;
+
+public class LabelProgressBar extends JLabel {
+
+	private int min;
+	private int max;
+	private int progress;
+	private int loadWidth;
+	private Color loadColor;
+	
+	public LabelProgressBar(int min, int max, int loadWidth) {
+		super();
+		this.min = min;
+		this.max = max;
+		progress = 0;
+		this.loadWidth = loadWidth;
+		setOpaque(false);
+	}
+	
+	public void setProgress(int progress) {
+		this.progress = progress;
+		this.repaint();
+	}
+	
+	public void setLoadColor(Color c) {
+		this.loadColor = c;
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		g.setColor(getBackground());
+		g2d.fillRect(0, 0, getWidth(), getHeight());
+		g.setColor(loadColor);
+		g2d.fillRect(0, 0, (int) (((1.0 * progress) / max ) * getWidth()), getHeight());
+		super.paintComponent(g);
+	}
+	
+}
--- src/main/java/net/technicpack/launcher/ui/controls/DownloadHeaderTab.java
+++ src/main/java/net/technicpack/launcher/ui/controls/DownloadHeaderTab.java
@@ -40,31 +40,33 @@ public class DownloadHeaderTab extends HeaderTab {
 	protected void paintComponent(Graphics g) {
 		char[] arr = Integer.toString(downloadsQueued).toCharArray();
 		super.paintComponent(g);
-		Graphics2D g2d = (Graphics2D) g;
-		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		Color old = g2d.getColor();
-		g2d.setColor(LauncherFrame.COLOR_CHARCOAL);
-		g2d.fillOval(D_COUNTER_MARGIN, this.getSize().height - D_COUNTER_MARGIN - D_COUNTER_SIZE, 
-				D_COUNTER_SIZE, D_COUNTER_SIZE);
-
-		int i = 0;
-		for (int downTemp = downloadsQueued / 10; downTemp != 0; downTemp /= 10) {
-			g2d.fillRect(D_COUNTER_MARGIN + D_COUNTER_SIZE / 2 + i * D_COUNTER_EXP, 
-					this.getSize().height - D_COUNTER_MARGIN - D_COUNTER_SIZE,
-					D_COUNTER_EXP, D_COUNTER_SIZE);
-			i++;
-		}
-		
-		if (i != 0) { 
-			g2d.fillOval(D_COUNTER_MARGIN + D_COUNTER_EXP * i, this.getSize().height - D_COUNTER_MARGIN - D_COUNTER_SIZE, 
+		if (downloadsQueued > 0) {
+			Graphics2D g2d = (Graphics2D) g;
+			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			Color old = g2d.getColor();
+			g2d.setColor(LauncherFrame.COLOR_CHARCOAL);
+			g2d.fillOval(D_COUNTER_MARGIN, this.getSize().height - D_COUNTER_MARGIN - D_COUNTER_SIZE, 
 					D_COUNTER_SIZE, D_COUNTER_SIZE);
+	
+			int i = 0;
+			for (int downTemp = downloadsQueued / 10; downTemp != 0; downTemp /= 10) {
+				g2d.fillRect(D_COUNTER_MARGIN + D_COUNTER_SIZE / 2 + i * D_COUNTER_EXP, 
+						this.getSize().height - D_COUNTER_MARGIN - D_COUNTER_SIZE,
+						D_COUNTER_EXP, D_COUNTER_SIZE);
+				i++;
+			}
+			
+			if (i != 0) { 
+				g2d.fillOval(D_COUNTER_MARGIN + D_COUNTER_EXP * i, this.getSize().height - D_COUNTER_MARGIN - D_COUNTER_SIZE, 
+						D_COUNTER_SIZE, D_COUNTER_SIZE);
+			}
+			
+			g2d.setColor(old);
+			
+			Font f = g2d.getFont();
+			g2d.setFont(new Font(f.getName(), Font.PLAIN, (int)(f.getSize() * .5)));
+			g2d.drawChars(arr, 0, arr.length, 10, this.getSize().height - 8);
 		}
-		
-		g2d.setColor(old);
-		
-		Font f = g2d.getFont();
-		g2d.setFont(new Font(f.getName(), Font.PLAIN, (int)(f.getSize() * .5)));
-		g2d.drawChars(arr, 0, arr.length, 10, this.getSize().height - 8);
 	}
 
 }
