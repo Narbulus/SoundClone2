5b05bc299b95945872baa80453bdd54d1ee656f2
Cameron Hardin <hardincm@cs.washington.edu>
2015-04-11 20:34:43 -0700

    gutting still

--- src/main/java/com/brassbeluga/launcher/LauncherMain.java
+++ src/main/java/com/brassbeluga/launcher/LauncherMain.java
@@ -18,87 +18,33 @@
 
 package com.brassbeluga.launcher;
 
-import java.awt.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.IOException;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.ArrayList;
 import java.util.Locale;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.swing.ToolTipManager;
 import javax.swing.UIManager;
 
 import net.technicpack.autoupdate.IBuildNumber;
 import net.technicpack.autoupdate.Relauncher;
-import net.technicpack.autoupdate.http.HttpUpdateStream;
-import net.technicpack.launchercore.auth.IUserStore;
-import net.technicpack.launchercore.auth.IUserType;
-import net.technicpack.launchercore.auth.UserModel;
-import net.technicpack.launchercore.exception.DownloadException;
-import net.technicpack.launchercore.image.ImageRepository;
-import net.technicpack.launchercore.image.face.CrafatarFaceImageStore;
-import net.technicpack.launchercore.image.face.WebAvatarImageStore;
 import net.technicpack.launchercore.install.LauncherDirectories;
-import net.technicpack.launchercore.install.ModpackInstaller;
-import net.technicpack.launchercore.launch.java.JavaVersionRepository;
-import net.technicpack.launchercore.launch.java.source.FileJavaSource;
-import net.technicpack.launchercore.launch.java.source.InstalledJavaSource;
 import net.technicpack.launchercore.logging.BuildLogFormatter;
 import net.technicpack.launchercore.logging.RotatingFileHandler;
-import net.technicpack.launchercore.mirror.MirrorStore;
-import net.technicpack.launchercore.mirror.secure.rest.JsonWebSecureMirror;
-import net.technicpack.launchercore.modpacks.ModpackModel;
-import net.technicpack.launchercore.modpacks.resources.PackImageStore;
-import net.technicpack.launchercore.modpacks.resources.PackResourceMapper;
-import net.technicpack.launchercore.modpacks.resources.resourcetype.BackgroundResourceType;
-import net.technicpack.launchercore.modpacks.resources.resourcetype.IModpackResourceType;
-import net.technicpack.launchercore.modpacks.resources.resourcetype.IconResourceType;
-import net.technicpack.launchercore.modpacks.resources.resourcetype.LogoResourceType;
-import net.technicpack.launchercore.modpacks.sources.IAuthoritativePackSource;
-import net.technicpack.launchercore.modpacks.sources.IInstalledPackRepository;
-import net.technicpack.minecraftcore.launch.MinecraftLauncher;
-import net.technicpack.minecraftcore.mojang.auth.AuthenticationService;
-import net.technicpack.minecraftcore.mojang.auth.MojangUser;
-import net.technicpack.platform.IPlatformApi;
-import net.technicpack.platform.IPlatformSearchApi;
-import net.technicpack.platform.PlatformPackInfoRepository;
-import net.technicpack.platform.cache.ModpackCachePlatformApi;
-import net.technicpack.platform.http.HttpPlatformApi;
-import net.technicpack.platform.http.HttpPlatformSearchApi;
-import net.technicpack.platform.io.AuthorshipInfo;
-import net.technicpack.solder.ISolderApi;
-import net.technicpack.solder.cache.CachedSolderApi;
-import net.technicpack.solder.http.HttpSolderApi;
 import net.technicpack.ui.components.Console;
 import net.technicpack.ui.components.ConsoleFrame;
 import net.technicpack.ui.components.ConsoleHandler;
 import net.technicpack.ui.components.LoggerOutputStream;
-import net.technicpack.ui.controls.installation.SplashScreen;
 import net.technicpack.ui.lang.ResourceLoader;
 import net.technicpack.utilslib.OperatingSystem;
 import net.technicpack.utilslib.Utils;
 
 import com.beust.jcommander.JCommander;
 import com.brassbeluga.launcher.autoupdate.CommandLineBuildNumber;
-import com.brassbeluga.launcher.autoupdate.TechnicRelauncher;
 import com.brassbeluga.launcher.autoupdate.VersionFileBuildNumber;
-import com.brassbeluga.launcher.io.TechnicAvatarMapper;
-import com.brassbeluga.launcher.io.TechnicFaceMapper;
-import com.brassbeluga.launcher.io.TechnicInstalledPackStore;
 import com.brassbeluga.launcher.io.TechnicLauncherDirectories;
-import com.brassbeluga.launcher.io.TechnicUserStore;
-import com.brassbeluga.launcher.launch.Installer;
 import com.brassbeluga.launcher.settings.SettingsFactory;
 import com.brassbeluga.launcher.settings.StartupParameters;
 import com.brassbeluga.launcher.settings.TechnicSettings;
@@ -156,38 +102,8 @@ public class LauncherMain {
 			buildNumber = new VersionFileBuildNumber(resources);
 
 		setupLogging(directories, resources, buildNumber);
+		startLauncher(resources);
 
-		String launcherBuild = buildNumber.getBuildNumber();
-		int build = -1;
-
-		try {
-			build = Integer.parseInt((new VersionFileBuildNumber(resources))
-					.getBuildNumber());
-		} catch (NumberFormatException ex) {
-			// This is probably a debug build or something, build number is
-			// invalid
-		}
-
-		Relauncher launcher = new TechnicRelauncher(new HttpUpdateStream(
-				"http://api.technicpack.net/launcher/"),
-				settings.getBuildStream() + "4", build, directories, resources,
-				params);
-
-		try {
-			if (launcher.runAutoUpdater())
-				startLauncher(settings, params, directories, resources,
-						buildNumber);
-		} catch (InterruptedException e) {
-			// Canceled by user
-		} catch (DownloadException e) {
-			// JOptionPane.showMessageDialog(null,
-			// resources.getString("launcher.updateerror.download",
-			// pack.getDisplayName(), e.getMessage()),
-			// resources.getString("launcher.installerror.title"),
-			// JOptionPane.WARNING_MESSAGE);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 
 	private static void setupLogging(LauncherDirectories directories,
@@ -229,156 +145,23 @@ public class LauncherMain {
 			public void uncaughtException(Thread t, Throwable e) {
 				e.printStackTrace();
 				logger.log(Level.SEVERE, "Unhandled Exception in " + t, e);
-
-				// if (errorDialog == null) {
-				// LauncherFrame frame = null;
-				//
-				// try {
-				// frame = Launcher.getFrame();
-				// } catch (Exception ex) {
-				// //This can happen if we have a very early crash- before
-				// Launcher initializes
-				// }
-				//
-				// errorDialog = new ErrorDialog(frame, e);
-				// errorDialog.setVisible(true);
-				// }
 			}
 		});
 	}
 
-	private static void startLauncher(final TechnicSettings settings,
-			StartupParameters startupParameters,
-			LauncherDirectories directories, ResourceLoader resources,
-			IBuildNumber buildNumber) throws Exception {
-		System.setProperty("xr.load.xml-reader",
-				"org.ccil.cowan.tagsoup.Parser");
+	private static void startLauncher(ResourceLoader resources) {
 
-		Utils.getLogger().info(
-				"OS: "
-						+ System.getProperty("os.name").toLowerCase(
-								Locale.ENGLISH));
-		Utils.getLogger().info(
-				"Identified as "
-						+ OperatingSystem.getOperatingSystem().getName());
+		Utils.getLogger().info("OS: " + System.getProperty("os.name").toLowerCase(Locale.ENGLISH));
+		Utils.getLogger().info("Identified as " + OperatingSystem.getOperatingSystem().getName());
 
-		final SplashScreen splash = new SplashScreen(
-				resources.getImage("launch_splash.png"), 0);
-		Color bg = Color.RED;
-		splash.getContentPane().setBackground(
-				new Color(bg.getRed(), bg.getGreen(), bg.getBlue(), 255));
-		splash.pack();
-		splash.setLocationRelativeTo(null);
-		splash.setVisible(true);
-
-		boolean loadedAether = false;
-
+		DownloadLikes downloader = null;
 		try {
-			if (Class
-					.forName(
-							"org.apache.maven.repository.internal.MavenRepositorySystemUtils",
-							false, ClassLoader.getSystemClassLoader()) != null) {
-				loadedAether = true;
-			}
-		} catch (ClassNotFoundException ex) {
-			// Aether is not loaded
+			downloader = new DownloadLikes(resources);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 
-		if (!loadedAether) {
-			File launcherAssets = new File(directories.getAssetsDirectory(),
-					"launcher");
-
-			File aether = new File(launcherAssets, "aether-dep.jar");
-
-			try {
-				Method m = URLClassLoader.class.getDeclaredMethod("addURL",
-						URL.class);
-				m.setAccessible(true);
-				m.invoke(ClassLoader.getSystemClassLoader(), aether.toURI()
-						.toURL());
-			} catch (NoSuchMethodException ex) {
-				ex.printStackTrace();
-			} catch (InvocationTargetException ex) {
-				ex.printStackTrace();
-			} catch (IllegalAccessException ex) {
-				ex.printStackTrace();
-			} catch (MalformedURLException ex) {
-				ex.printStackTrace();
-			}
-		}
-
-		JavaVersionRepository javaVersions = new JavaVersionRepository();
-		(new InstalledJavaSource()).enumerateVersions(javaVersions);
-		FileJavaSource javaVersionFile = FileJavaSource.load(new File(settings
-				.getTechnicRoot(), "javaVersions.json"));
-		javaVersionFile.enumerateVersions(javaVersions);
-		javaVersions.selectVersion(settings.getJavaVersion(),
-				settings.getJavaBitness());
-
-		IUserStore<MojangUser> users = TechnicUserStore.load(new File(
-				directories.getLauncherDirectory(), "users.json"));
-		UserModel userModel = new UserModel(users, new AuthenticationService());
-
-		MirrorStore mirrorStore = new MirrorStore(userModel);
-		mirrorStore.addSecureMirror("mirror.technicpack.net",
-				new JsonWebSecureMirror("http://mirror.technicpack.net/",
-						"mirror.technicpack.net"));
-
-		IModpackResourceType iconType = new IconResourceType();
-		IModpackResourceType logoType = new LogoResourceType();
-		IModpackResourceType backgroundType = new BackgroundResourceType();
-
-		PackResourceMapper iconMapper = new PackResourceMapper(directories,
-				resources.getImage("icon.png"), iconType);
-		ImageRepository<ModpackModel> iconRepo = new ImageRepository<ModpackModel>(
-				iconMapper,
-				new PackImageStore(iconType, mirrorStore, userModel));
-		ImageRepository<ModpackModel> logoRepo = new ImageRepository<ModpackModel>(
-				new PackResourceMapper(directories,
-						resources.getImage("modpack/ModImageFiller.png"),
-						logoType), new PackImageStore(logoType, mirrorStore,
-						userModel));
-		ImageRepository<ModpackModel> backgroundRepo = new ImageRepository<ModpackModel>(
-				new PackResourceMapper(directories, null, backgroundType),
-				new PackImageStore(backgroundType, mirrorStore, userModel));
-
-		ImageRepository<IUserType> skinRepo = new ImageRepository<IUserType>(
-				new TechnicFaceMapper(directories, resources),
-				new CrafatarFaceImageStore("http://crafatar.com/", mirrorStore));
-
-		ImageRepository<AuthorshipInfo> avatarRepo = new ImageRepository<AuthorshipInfo>(
-				new TechnicAvatarMapper(directories, resources),
-				new WebAvatarImageStore(mirrorStore));
-
-		HttpSolderApi httpSolder = new HttpSolderApi(settings.getClientId(),
-				userModel);
-		ISolderApi solder = new CachedSolderApi(directories, httpSolder,
-				60 * 60);
-		HttpPlatformApi httpPlatform = new HttpPlatformApi(
-				"http://api.technicpack.net/", mirrorStore,
-				buildNumber.getBuildNumber());
-
-		IPlatformApi platform = new ModpackCachePlatformApi(httpPlatform,
-				60 * 60, directories);
-		IPlatformSearchApi platformSearch = new HttpPlatformSearchApi(
-				"http://api.technicpack.net/", buildNumber.getBuildNumber());
-
-		IInstalledPackRepository packStore = TechnicInstalledPackStore
-				.load(new File(directories.getLauncherDirectory(),
-						"installedPacks"));
-		IAuthoritativePackSource packInfoRepository = new PlatformPackInfoRepository(
-				platform, solder);
-
-		MinecraftLauncher launcher = new MinecraftLauncher(platform,
-				directories, userModel, settings.getClientId(), javaVersions);
-		ModpackInstaller modpackInstaller = new ModpackInstaller(platform,
-				settings.getClientId());
-		Installer installer = new Installer(startupParameters, mirrorStore,
-				directories, modpackInstaller, launcher, settings, iconMapper);
-
-		DownloadLikes downloader = new DownloadLikes(resources);
-
-		
-		final LauncherFrame frame = new LauncherFrame(resources, downloader);
+		// Start the launcher!
+		new LauncherFrame(resources, downloader);
 	}
 }
