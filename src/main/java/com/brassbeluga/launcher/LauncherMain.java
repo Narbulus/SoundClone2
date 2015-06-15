50ea64523cf1586cbe18a1b6f3010395d60b07b2
hardincm2 <hardincm2@gmail.com>
2015-06-15 16:06:20 -0700

    rippin and roarin

--- src/main/java/com/brassbeluga/launcher/LauncherMain.java
+++ src/main/java/com/brassbeluga/launcher/LauncherMain.java
@@ -18,26 +18,46 @@
 
 package com.brassbeluga.launcher;
 
-import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import com.brassbeluga.launcher.ui.LauncherFrame;
 
 public class LauncherMain {
-
+	private static final String BASE_URL = "http://www.brassbeluga.com/soundclone/";
 
 	public static void main(String[] args) throws Exception {
-		startLauncher();
+		if (checkUpdate()) {
+			Runtime.getRuntime().exec("java -jar updater.jar");
+			System.exit(0);
+		}
+		new LauncherFrame();
 	}
 
-	private static void startLauncher() {
-		/*try {
-			Runtime.getRuntime().exec("java -jar soundclone-1.0-0.jar");
-			System.exit(0);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+	private static boolean checkUpdate() {
+		boolean update = false;
+		
+		try {
+			String version = new String(Files.readAllBytes(Paths.get("version")), "UTF-8").trim();
+			
+			String url = BASE_URL + "update.php?version=" + version;
+				
+			URL obj = new URL(url);
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+	 
+			con.setRequestMethod("GET");
+			int responseCode = con.getResponseCode();
+			
+			if (con.getHeaderField("Update-Needed").equals("1")) {
+				update = true;
+			}
+		
+		} catch (Exception e) {
 			e.printStackTrace();
-		}*/
-		// Start the launcher!
-		new LauncherFrame();
+		}
+		
+		return update;
 	}
 }
